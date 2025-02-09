6c1f6017c3f4f3cf5033058cc3253838b2906eff
hemanthhhhhh <krishnahemant2005@gmail.com>
2025-02-09 16:16:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const httpProxy = require('http-proxy')
+
+const app = express()
+const PORT = 8000
+
+const basePath = 'https://vercel-cloud-bucket.s3.ap-south-1.amazonaws.com/__outputs'
+const proxy = httpProxy.createProxy()
+
+app.use((req, res) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+
+    const resolvesTo = `${basePath}/${subdomain}`
+
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
+})
+
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/')
+        proxyReq.path += 'index.html'
+})
+
+app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`))
